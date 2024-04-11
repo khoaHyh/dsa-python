ec9ea1aff82d2eebee87d8c186544fe78e0d5b34
khoaHyh <khoahuynh.toronto@gmail.com>
2024-04-10 22:50:12 -0400

    add poll and heapifyDown

--- MinHeap.py
+++ MinHeap.py
@@ -15,14 +15,32 @@ class MinHeap:
     def __str__(self):
         return f"{self.items}"
 
+    def _getLeftChildIndex(self, parentIndex):
+        return 2 * parentIndex + 1
+
+    def _getRightChildIndex(self, parentIndex):
+        return 2 * parentIndex + 2
+
     def _getParentIndex(self, childIndex: int):
         # [0,1,2,3,4,5,6] - 1 and 2 have child nodes
         # 3 and 4's parent is 1, 5 and 6's parent is 2
         return (childIndex - 1) // 2
 
+    def _hasLeftChild(self, index: int):
+        return self._getLeftChildIndex(index) < self.size
+
+    def _hasRightChild(self, index: int):
+        return self._getRightChildIndex(index) < self.size
+
     def _hasParent(self, index: int):
         # Only scenario where a value would not have a parent is the root element.
         return self._getParentIndex(index) >= 0
+
+    def _leftChildValue(self, index: int):
+        return self.items[self._getLeftChildIndex(index)]
+
+    def _rightChildValue(self, index: int):
+        return self.items[self._getRightChildIndex(index)]
 
     def _parentValue(self, index: int):
         return self.items[self._getParentIndex(index)]
@@ -32,6 +50,15 @@ class MinHeap:
             raise EmptyHeapError("peek")
 
         return self.items[0]
+
+    def poll(self):
+        if (self.size == 0):
+            raise EmptyHeapError("peek")
+
+        min_item = self.items[0]
+        self.items[0] = self.items.pop()
+        self.heapifyDown()
+        return min_item
 
     def _swap(self, indexOne:int, indexTwo:int):
         temp = self.items[indexOne]
@@ -52,6 +79,24 @@ class MinHeap:
             # swap elements and update the index so we can move up the heap and compare again
             self._swap(index, self._getParentIndex(index))
             index = self._getParentIndex(index)
+
+    def heapifyDown(self):
+        index = 0
+
+        while self._hasLeftChild(index):
+            smallerChildIndex = self._getLeftChildIndex(index)
+
+            # Check if there is a right child and see if the right child is smaller than the left child
+            if self._hasRightChild(index) and (self._rightChildValue(index) < self._leftChildValue(index)):
+                smallerChildIndex = self._getRightChildIndex(index)
+
+            if self.items[index] < self.items[smallerChildIndex]:
+                break
+            else:
+                self._swap(index, smallerChildIndex)
+            
+            index = smallerChildIndex
+
 
     # TODO: add method to get heapify the entire heap
 
